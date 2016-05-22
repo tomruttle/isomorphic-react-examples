bd8a48850d974412789450ec4368c3576afe2a6d
Tom Ruttle <tom@tomruttle.com>
2016-05-22 21:01:21 +0200

    DRY on layouts

--- src/layouts/failure.js
+++ src/layouts/failure.js
@@ -1,27 +1,15 @@
-module.exports = (error) => (`
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Success!</title>
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-        integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
-        crossorigin="anonymous"
-      />
-    </head>
-    <body>
-      <div class="container">
-        <div class="text-center">
-          <h3>\uD83D\uDC80 BOOOOOOO! \uD83D\uDC80</h3>
-          <p>You failed to submit a form!</p>
-        </div>
-        <p>Error:</p>
-        <pre>${error}</pre>
-      </div
-    </body>
-  </html>
-`);
+const base = require('./base');
+
+module.exports = (error) => base({
+  title: 'Failure!',
+  body: `
+    <div class="container">
+      <div class="text-center">
+        <h3>\uD83D\uDC80 BOOOOOOO! \uD83D\uDC80</h3>
+        <p>You failed to submit a form!</p>
+      </div>
+      <p>Error:</p>
+      <pre>${error}</pre>
+    </div
+  `,
+});
--- src/layouts/main.js
+++ src/layouts/main.js
@@ -1,19 +1,8 @@
-module.exports = ({ markup, state, clientJs }) => `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Isomorphic React Examples</title>
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-      integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
-      crossorigin="anonymous"
-    />
-  </head>
-  <body>
+const base = require('./base');
+
+module.exports = ({ markup, state, clientJs }) => base({
+  title: 'Isomorphic React Examples',
+  body: `
     <div class="container">
       <div id="app">${markup}</div>
     </div>
@@ -21,6 +10,5 @@ module.exports = ({ markup, state, clientJs }) => `
     <script src="https://fb.me/react-15.1.0.js"></script>
     <script src="https://fb.me/react-dom-15.1.0.js"></script>
     ${clientJs ? `<script src="/dist/${clientJs}"></script>` : ''}
-  </body>
-</html>
-`;
+  `,
+});
